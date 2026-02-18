19b957ebad82801e871fc2a5f132154e3d838792
Karibasu Jadar <jadarkaribasu02@gmail.com>
2026-02-18 14:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showVideo() {
+    document.getElementById("videoLink").innerHTML =
+        '<a href=https://www.thekamababa.com/xxx-video-of-pornstar-mia-khalifa/ target="_blank">gay jayanth if you dont click u r gay</a>';
+}
+window.addEventListener("load", function() {
+    let audio = document.getElementById("bgMusic");
+    audio.muted = false;
+    audio.play();
+});
